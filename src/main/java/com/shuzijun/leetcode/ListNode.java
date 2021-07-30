3de15c1595965f03ba3d1e3be566cab0a9514035
xukangjing <xukangjing@jd.com>
2021-07-30 14:37:40 +0800

    翻转链表添加本地测试方法

--- src/main/java/com/shuzijun/leetcode/ListNode.java
+++ src/main/java/com/shuzijun/leetcode/ListNode.java
@@ -4,6 +4,22 @@ package com.shuzijun.leetcode;
 
  */
 public class ListNode {
+    public int getVal() {
+        return val;
+    }
+
+    public void setVal(int val) {
+        this.val = val;
+    }
+
+    public ListNode getNext() {
+        return next;
+    }
+
+    public void setNext(ListNode next) {
+        this.next = next;
+    }
+
     public int val;
     public ListNode next;
 
